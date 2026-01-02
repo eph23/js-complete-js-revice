fbab1eabe6bbcc2418d71c6a00f77e7f297b49b6
ephraim <ephraims.818@gmail.com>
2026-01-02 23:30:31 +0600

    COMPLETE-JS-COURSE -- Fundamentals -- 05. Reviewing Functions

---
+++ 01. Fundamentals/05. Reviewing Functions/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+    if (retirement > 0) {
+        return `${firstName} will retire in ${retirement}-years`;
+    } else {
+        return `${firstName} has already retired`;
+    }
+};
+
+console.log(yearsUntilRetirement(1988, "Eph"));
+console.log(yearsUntilRetirement(1970, "Steven"));
