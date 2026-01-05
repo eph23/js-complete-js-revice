3b85a967b556578b7f6c8d50b3eaf48fd2578ef8
ephraim <ephraims.818@gmail.com>
2026-01-05 20:05:55 +0600

    COMPLETE-JS-COURSE -- DOM -- GUESS-MY-NUMBER -- 02. Handling Click Events

--- 02. DOM/01. Guess My Number/guess-my-number/script.js
+++ 02. DOM/01. Guess My Number/guess-my-number/script.js
@@ -1,8 +1,12 @@
 'use strict';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+const message = document.querySelector('.message');
 
-document.querySelector('.guess').value = 23;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
 
-console.log(document.querySelector('.guess').value);
+  if (!guess) {
+    message.textContent = '🚫 No number!';
+  }
+});
