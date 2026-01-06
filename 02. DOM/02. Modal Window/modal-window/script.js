e4ee31d50c09af67968cf5bebcadfc5900552e1d
ephraim <ephraims.818@gmail.com>
2026-01-07 03:51:10 +0600

    COMPLETE-JS-COURSE -- DOM -- MODAL-WINDOW -- 02. Working With CSS Classes

--- 02. DOM/02. Modal Window/modal-window/script.js
+++ 02. DOM/02. Modal Window/modal-window/script.js
@@ -6,5 +6,16 @@ const closeBtn = document.querySelector('.close-modal');
 const showBtns = document.querySelectorAll('.show-modal');
 
 for (let i = 0; i < showBtns.length; i++) {
-  console.log(showBtns[i].textContent);
+  showBtns[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
 }
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+closeBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
