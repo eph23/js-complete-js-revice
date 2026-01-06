fe253b26ac77f37c555e41c0d45b17dd3f318b9b
ephraim <ephraims.818@gmail.com>
2026-01-06 11:07:25 +0600

    COMPLETE-JS-COURSE -- DOM -- GUESS-MY-NUMBER -- 03. Implementing the Game Logic

--- 02. DOM/01. Guess My Number/guess-my-number/script.js
+++ 02. DOM/01. Guess My Number/guess-my-number/script.js
@@ -1,4 +1,11 @@
 'use strict';
+const secretNumber = Math.floor(Math.random() * 20) + 1;
+console.log(secretNumber);
+
+let score = 20;
+document.querySelector('.score').textContent = score;
+
+document.querySelector('.number').textContent = secretNumber;
 
 const message = document.querySelector('.message');
 
@@ -8,5 +15,25 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     message.textContent = '🚫 No number!';
+  } else if (guess === secretNumber) {
+    message.textContent = '🍾 Correct Number! 🎉';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      message.textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      message.textContent = '🤯 YOU LOST THE GAME...';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      message.textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      message.textContent = '🤯 YOU LOST THE GAME...';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
