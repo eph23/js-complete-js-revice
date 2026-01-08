df6e770ad16a2054649c0c73e0479e280af799ac
ephraim <ephraims.818@gmail.com>
2026-01-09 02:58:56 +0600

    COMPLETE-JS-COURSE -- DOM -- PIG-GAME -- 03. Switching the Active Player

--- 02. DOM/03. Pig Game/pig-game/script.js
+++ 02. DOM/03. Pig Game/pig-game/script.js
@@ -1,7 +1,12 @@
 'use strict';
 
 let currentScore = 0;
+let activePlayer = 0;
 
+const scores = [0, 0];
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.querySelector('#current--0');
@@ -25,7 +30,14 @@ btnRoll.addEventListener('click', function () {
 
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    document;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
