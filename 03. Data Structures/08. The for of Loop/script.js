fe1a7af68bf0efe3fd3a06d10bcb64a8292c9a01
ephraim <ephraims.818@gmail.com>
2026-01-12 02:14:13 +0600

    COMPLETE-JS-COURSE -- Data Structures -- 08. The for of Loop

---
+++ 03. Data Structures/08. The for of Loop/script.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  // Destructuring returned value (array destructuring)
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  // Destructuring function arguments value (object destructuring)
+  orderDelivery: function ({
+    time = '20:00',
+    address,
+    mainIndex = 1,
+    starterIndex = 0,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered on ${time} to ${address}`
+    );
+  },
+
+  // Spread operator as function argument
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
+    );
+  },
+
+  // Rest parameters
+  orderPizza: function (mainIng, ...otherIng) {
+    console.log(mainIng);
+    console.log(otherIng);
+  },
+};
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+// The for of loop
+for (const item of menu) {
+  console.log(item);
+}
+
+// Object.entries
+for (const [index, item] of menu.entries()) {
+  console.log(`${index + 1}: ${item}`);
+}
