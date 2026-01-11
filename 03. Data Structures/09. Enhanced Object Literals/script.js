5f67bc398f978cad8890004da48ccb73aef70f53
ephraim <ephraims.818@gmail.com>
2026-01-12 02:20:06 +0600

    COMPLETE-JS-COURSE -- Data Structures -- 09. Enhanced Object Literals

---
+++ 03. Data Structures/09. Enhanced Object Literals/script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const weekDays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekDays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekDays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours,
+
+  // Destructuring returned value (array destructuring)
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  // Destructuring function arguments value (object destructuring)
+  orderDelivery({ time = '20:00', address, mainIndex = 1, starterIndex = 0 }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered on ${time} to ${address}`
+    );
+  },
+
+  // Spread operator as function argument
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
+    );
+  },
+
+  // Rest parameters
+  orderPizza(mainIng, ...otherIng) {
+    console.log(mainIng);
+    console.log(otherIng);
+  },
+};
