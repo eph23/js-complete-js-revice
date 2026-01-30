a01dde0b74fa72063c8007c2340214ba4568d14c
ephraim <ephraims.818@gmail.com>
2026-01-31 01:30:13 +0600

    COMPLETE-JS-COURSE -- Arrays -- 17. Summery

---
+++ 05. Arrays/17. Summery/script.js
@@ -0,0 +1,126 @@
+'use strict';
+
+/* ******************************************** */
+/*            TO MUTE ORIGINAL ARRAY            */
+// Add to original
+Array.push();
+Array.unshift();
+
+// Remove from original
+Array.pop();
+Array.shift();
+Array.splice();
+
+// Others:
+Array.reverse();
+Array.sort();
+Array.fill();
+/* ******************************************** */
+
+/* ******************************************** */
+/*         A NEW ARRAY BASED ON ORIGINAL        */
+// Same as original
+Array.map(); //Loop;
+
+// Filter using condition
+Array.filter();
+
+// Taking portion of original
+Array.slice();
+
+// With one item replaced
+Array.with();
+
+// Flattened
+Array.flat();
+Array.flatMap();
+
+// Reversed
+Array.toReversed();
+
+// Sorted
+Array.toSorted();
+
+// With deleted items
+Array.toSplice();
+
+// Joining two arrays
+Array.concat();
+
+/* ******************************************** */
+
+/* ******************************************** */
+/*                AN ARRAY INDEX                */
+// Based on value
+Array.indexOf();
+
+// Based on test condition
+Array.findIndex;
+Array.findLastIndex;
+
+/* ******************************************** */
+
+/* ******************************************** */
+/*               AN ARRAY ELEMENT               */
+// Based on test condition
+Array.find();
+Array.findLast();
+
+// Based on position
+Array.at();
+
+/* ******************************************** */
+
+/* ******************************************** */
+/*            KNOW IF ARRAY INCLUDES            */
+// Based on value
+Array.includes();
+
+// Based on test condition
+Array.some();
+Array.every();
+/* ******************************************** */
+
+/* ******************************************** */
+/*                 A NEW STRING                 */
+// Based on separator
+Array.join();
+
+/* ******************************************** */
+
+/* ******************************************** */
+/*                 TO TRANSFORM TO VALUE                */
+// Based on separator
+Array.reduce();
+
+/* ******************************************** */
+
+/* ******************************************** */
+/*                 LOOP ARRAY                */
+// Based on separator
+Array.forEach();
+
+/* ******************************************** */
+
+
+/* ******************************************** */
+/*              MORE ARRAY TOOLS AND TECHNIQUES              */
+// Grouping an array by categories
+object.groupBy()
+
+// Creating a bew array
+Array.from()
+
+// Creating a new array from scratch with n empty positions
+new Array(n)
+
+// Joining two or more arrays
+[...array1,...array2]
+
+// Creating a new array containing unique values
+[...new Set(arr)]
+
+// Creating a new array containing unique elements that are present in both arrays
+[...new Set(array1).intersection(new Set(array2))]
+
+/* ******************************************** */
