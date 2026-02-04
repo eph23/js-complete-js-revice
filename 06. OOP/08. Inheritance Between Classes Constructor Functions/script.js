1fb83052bb492b956da4dc00d3eed6ba4d2f3080
ephraim <ephraims.818@gmail.com>
2026-02-05 05:15:26 +0600

    COMPLETE-JS-COURSE -- OOP -- 08. Inheritance Between Classes Constructor Functions

---
+++ 06. OOP/08. Inheritance Between Classes Constructor Functions/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const eph = new Person('Eph', 1988);
+console.log(eph);
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+// Linking prototypes
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName}, I study ${this.course}`);
+};
+
+const mike = new Student('Mike', 2020, 'Computer Science');
+console.log(mike);
+mike.introduce();
+
+console.log(mike.__proto__);
+console.log(mike.__proto__.__proto__);
+
+Student.prototype.constructor = Student;
+console.dir(Student.prototype.constructor);
